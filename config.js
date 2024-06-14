c18a04c6e6bd82665c710c79344a668f2579dbc6
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-06-14 23:45:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,6 @@ import cheerio from "cheerio"
 import fetch from "node-fetch"
 import axios from "axios"
 import moment from "moment-timezone"
-import { es as esDefault, en as enDefault } from "./lib/multi-language/_default.js"
-import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
